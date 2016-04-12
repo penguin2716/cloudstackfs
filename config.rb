80813c9d517b3f9919d578e75b72c7280a2b9362
Takuma Nakajima <penguin.trance.2716@gmail.com>
2016-04-13 02:35:30 +0900

    parameterize version number of cloudstack

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
-API_SOURCE_URL = 'https://cloudstack.apache.org/docs/api/apidocs-4.5/TOC_Root_Admin.html'
+CLOUDSTACK_VERSION = "4.5"
+API_SOURCE_URL = "https://cloudstack.apache.org/docs/api/apidocs-#{CLOUDSTACK_VERSION}/TOC_Root_Admin.html"
 CACHE_DIR = File.expand_path(File.join(File.dirname(__FILE__), 'cache'))
 
 class APICaller
